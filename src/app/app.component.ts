db37a4e9b8d8f5a537c198af97bd9c126a8f7de7
Tolotra David <tolotradavid2@gmail.com>
2022-07-28 17:32:11 +0300

    Les observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -12,5 +13,17 @@ export class AppComponent implements OnInit {
       error: (err: unknown) => console.log(`Oups il y a eu une erreur ${err}`),
       complete: () => console.log(`terminé... plus rien`)
     };
+    const stream = new Observable(myObserver => {
+      myObserver.next('Boite 1');
+      myObserver.next('Boite 2');
+      myObserver.next('Boite 3');
+      myObserver.complete();
+    });
+
+    const subscription = stream.subscribe(
+      item => console.log(`Une boite arrive ${item}`),
+      err => console.log(`Erreur ${err}`),
+      () => console.log(`terminé... plus rien`)
+    );
   }
 }
